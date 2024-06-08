e6ce5818e8a60f10e648984c8447fbf13bacd4fa
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-08 22:38:57 +0200

    Challenge: Wrap the contents of the "van-tile" div in a   Link that sends the user to `/vans/${van-id-here}`.

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,4 +1,10 @@
 import React from "react"
+import { Link } from "react-router-dom"
+
+/**
+ * Challenge: Wrap the contents of the "van-tile" div in a 
+ * Link that sends the user to `/vans/${van-id-here}`.
+ */
 
 export default function Vans() {
     const [vans, setVans] = React.useState([])
@@ -11,15 +17,16 @@ export default function Vans() {
 
     const vanElements = vans.map(van => {
         return (
-        <div key={van.id} className="van-tile">
-        <img src={van.imageUrl} />
-        <div className="van-info">
-            <h3>{van.name}</h3>
-            <p>${van.price}<span>/day</span></p>
-        </div>
-        <i className={`van-type ${van.type} selected`}>{van.type}</i>
-        </div>
-            
+        <Link to={`/vans/${van.id}`}>
+            <div key={van.id} className="van-tile">
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </div>
+        </Link>   
         )
     })
     
