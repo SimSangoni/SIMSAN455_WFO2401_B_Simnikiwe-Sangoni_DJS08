5a6dd1a274e7a56e053bfff58ff9cfc1c2387e4d
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-08 22:21:18 +0200

    fetched van from vans api

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,10 +1,36 @@
-
+import React from "react"
 
 export default function Vans() {
+    const [vansData, setVansData] = React.useState({})
+
+    React.useEffect(function() {
+        console.log("Effect ran")
+        fetch("/api/vans")
+            .then(res => res.json())
+            .then(data => setVansData(data))
+    }, [])
+
+    const vanElement = vansData.vans.map(van => {
+        return (
+            <div className="van-tile">
+                <div >
+                    <img src={van.imageUrl}/>
+                    {van.name}
+                    {`$${van.price} /day`}
+                </div>
+                <button className={`van-type ${van.type}`}>{van.type}</button>
+            </div>
+            
+        )
+    })
+    
 
     return (
     <div className="van-list-container">
-        <h1>Vans page goes here 🚐</h1>
+        <h1>Explore our van options</h1>
+        <div className="van-list">
+            {vanElement}
+        </div>
     </div>
     )
   }
