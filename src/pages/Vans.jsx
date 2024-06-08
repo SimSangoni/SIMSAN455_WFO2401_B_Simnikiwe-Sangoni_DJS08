6ef7a10945d3d3063704a5364656cc1f20d3bae8
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-08 22:44:15 +0200

    added aria-labels

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,10 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-/**
- * Challenge: Wrap the contents of the "van-tile" div in a 
- * Link that sends the user to `/vans/${van-id-here}`.
- */
+
 
 export default function Vans() {
     const [vans, setVans] = React.useState([])
@@ -17,16 +14,20 @@ export default function Vans() {
 
     const vanElements = vans.map(van => {
         return (
-        <Link to={`/vans/${van.id}`}>
             <div key={van.id} className="van-tile">
-                <img src={van.imageUrl} />
-                <div className="van-info">
-                    <h3>{van.name}</h3>
-                    <p>${van.price}<span>/day</span></p>
-                </div>
-                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                <Link 
+                    to={`/vans/${van.id}`}
+                    aria-label={`View details for ${van.name}, 
+                    priced at $${van.price} per day`}>
+                        <img src={van.imageUrl} />
+                        <div className="van-info">
+                            <h3>{van.name}</h3>
+                            <p>${van.price}<span>/day</span></p>
+                        </div>
+                        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                </Link> 
             </div>
-        </Link>   
+          
         )
     })
     
