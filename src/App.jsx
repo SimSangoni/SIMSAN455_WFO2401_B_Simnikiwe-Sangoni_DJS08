743892d0fc9ed7133a785e29442386a10aca58ae
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-08 22:33:46 +0200

    created VanDetail component then added route, no link yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import {BrowserRouter, Route, Routes, Link} from 'react-router-dom'
+
+
 import Home from './pages/Home'
 import About from './pages/About'
 import Vans from './pages/Vans';
+import VanDetail from './pages/VanDetail';
+
+
 import './server';
 
 
@@ -21,6 +26,7 @@ function App() {
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
           <Route path='/vans' element={<Vans/>}/>
+          <Route path="/vans/:id" element={<VanDetail />} />
         </Routes>
       </BrowserRouter>
     
