a7ccc69aab304f46d18a83bb21419c781fe9cb52
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-09 00:50:46 +0200

    header component exported

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-<header>
-    <Link className="site-logo" to='/'>#VANLIFE</Link>
-    <nav>
-        <Link to='/host'>Host</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/vans'>Vans</Link>
-    </nav>
-</header>
+
+export default function Header(){
+
+    return (
+        <header>
+            <Link className="site-logo" to='/'>#VANLIFE</Link>
+            <nav>
+                <Link to='/host'>Host</Link>
+                <Link to='/about'>About</Link>
+                <Link to='/vans'>Vans</Link>
+            </nav>
+        </header>
+    )
+}
+
