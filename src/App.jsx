444ab249fb05898c589ce0be8feedfdce0908b80
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-08 21:36:42 +0200

    remaned vans list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import {BrowserRouter, Route, Routes, Link} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import Vans from './pages/VansList';
+import Vans from './pages/Vans';
 import './server';
 
 
