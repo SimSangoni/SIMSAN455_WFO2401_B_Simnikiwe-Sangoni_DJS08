add6e701ef688014817530fe02d4223de70c653c
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-09 01:00:10 +0200

    header it working using outlet

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import {BrowserRouter, Route, Routes, Link} from 'react-router-dom'
 
 
-import Home from './pages/Home'
-import About from './pages/About'
+import Home from './pages/Home';
+import About from './pages/About';
 import Vans from './pages/Vans';
 import VanDetail from './pages/VanDetail';
 import Host from './pages/Host';
+import Header from './components/Header';
+// import Layout from './components/Layout';
 
 
 import './server';
+
 
 
 function App() {
@@ -18,11 +21,14 @@ function App() {
 
     <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/host" element={<Host/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path='/vans' element={<Vans/>}/>
-          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route element={<Header />}>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/host" element={<Host/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path='/vans' element={<Vans/>}/>
+            <Route path="/vans/:id" element={<VanDetail />} />
+          </Route>
+          
         </Routes>
       </BrowserRouter>
     
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,1 +1,11 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+
+export default function Layout(){
+    return (
+        <div>
+            <h1>This is the Layout Component</h1>
+            <Outlet />
+        </div>
+    )
+}
