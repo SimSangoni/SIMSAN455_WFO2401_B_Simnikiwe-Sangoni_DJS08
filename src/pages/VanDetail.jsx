1b2a83620158cd3f06900d666a2249018999191f
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-08 23:00:18 +0200

    fetched the data for the van that has been selected

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -1,5 +1,18 @@
+import React from "react"
+import { useParams } from "react-router-dom"
 
 
 export default function VanDetail() {
-    return <h1>Van detail page goes here</h1>
+    const params = useParams()
+    
+    React.useEffect(()=>{
+        fetch(`/api/vans/${params.id}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        },
+        [params.id]
+    )
+    
+
+    return <h1>{`Van detail page goes here`} </h1>
 }
