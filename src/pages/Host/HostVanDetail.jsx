59723981f830e3ef475ac295de671bb18a3b4569
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-09 18:08:23 +0200

    added fetch request and image for selected host van

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function HostVanDetail(){
+    const {id} = useParams()
+    const [currentVan, setCurrentVan] = React.useState(null)
+    
+    React.useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+            .then(res => res.json())
+            .then(data => setCurrentVan(data.vans))
+    }, [])
+
     return (
-        <>
+        <div>
             <h1>Your Host Van Details page is here</h1>
-        </>
+            <img src={currentVan.imageUrl} />
+        </div>
     )
 }
