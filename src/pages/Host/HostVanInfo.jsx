8eb8fe096e57d873a0c56d3e92f64d5c1220d7ed
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-09 21:58:15 +0200

    added context to react

--- src/pages/Host/HostVanInfo.jsx
+++ src/pages/Host/HostVanInfo.jsx
@@ -1,9 +1,12 @@
-import React from "react"
+import React from "react";
+import { useOutletContext } from "react-router-dom";
 
 export default function HostVanInfo() {
+    const [currentVan] = useOutletContext();
     return (
         <div>
             <h2>Van info here</h2>
+            {currentVan.description}
         </div>
     )
 };
--- src/pages/Host/HostVanPricing.jsx
+++ src/pages/Host/HostVanPricing.jsx
@@ -1,9 +1,12 @@
-import React from "react"
+import React from "react";
+import { useOutletContext } from "react-router-dom";
 
 export default function HostVanPricing() {
+    const [currentVan] = useOutletContext();
     return (
         <div>
             <h2>Pricing view here</h2>
+            {currentVan.price}
         </div>
     )
 };
