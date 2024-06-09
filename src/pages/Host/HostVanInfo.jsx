04f228c9430c34e8b49eec5dcc392afbcd768411
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-09 22:03:04 +0200

    current van info loaded

--- src/pages/Host/HostVanInfo.jsx
+++ src/pages/Host/HostVanInfo.jsx
@@ -4,9 +4,11 @@ import { useOutletContext } from "react-router-dom";
 export default function HostVanInfo() {
     const {currentVan} = useOutletContext();
     return (
-        <div>
-            <h2>Van info here</h2>
-            {currentVan.description}
-        </div>
+        <section className="host-van-detail-info">
+            <h4>Name: <span>{currentVan.name}</span></h4>
+            <h4>Category: <span>{currentVan.type}</span></h4>
+            <h4>Description: <span>{currentVan.description}</span></h4>
+            <h4>Visibility: <span>Public</span></h4>
+        </section>
     )
 };
