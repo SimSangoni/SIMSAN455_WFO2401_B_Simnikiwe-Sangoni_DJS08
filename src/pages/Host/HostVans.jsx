1798f9167fad0780fe286df4352c1a641cd0bf7e
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-09 17:49:44 +0200

    added host van headers and divisions

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -10,11 +10,28 @@ export default function HostVans(){
             .then(data => setVans(data.vans))
     }, [])
 
-    
+    const hostVansEls = vans.map(van => (
+        <Link
+            to={`/host/vans/${van.id}`}
+            key={van.id}
+            className="host-van-link-wrapper"
+        >
+            <div className="host-van-single" key={van.id}>
+                <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+                <div className="host-van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}/day</p>
+                </div>
+            </div>
+        </Link>
+    ))
 
     return (
-        <>
-            <h1>Your Listed vans</h1>
-        </>
+        <section>
+            <h1 className="host-vans-title">Your listed vans</h1>
+            <div className="host-vans-list">
+                
+            </div>
+        </section>
     )
 } 
