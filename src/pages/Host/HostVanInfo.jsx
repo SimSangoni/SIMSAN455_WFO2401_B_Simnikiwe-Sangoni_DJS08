c8b11e305544d44839a204b5feb71e342996fdd7
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-09 21:20:11 +0200

    added stubbed out components for the 3 new host components

---
+++ src/pages/Host/HostVanInfo.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function HostVanInfo() {
+    return (
+        <div>
+            <h2>Van info here</h2>
+        </div>
+    )
+};
---
+++ src/pages/Host/HostVanPhotos.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function HostVanPhotos() {
+    return (
+        <div>
+            <h2>Photos view here</h2>
+        </div>
+    )
+};
