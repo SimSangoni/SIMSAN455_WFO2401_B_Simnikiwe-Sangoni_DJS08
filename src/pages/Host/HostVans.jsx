618db1fbad4cdcf1b0448babbaefe9dbb3a3cc62
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-09 17:47:47 +0200

    useEffect for host vans

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function HostVans(){
+    const [vans, setVans] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("/api/host/vans")
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
+    }, [])
+
+    
+
     return (
         <>
             <h1>Your Listed vans</h1>
         </>
     )
-}
+} 
