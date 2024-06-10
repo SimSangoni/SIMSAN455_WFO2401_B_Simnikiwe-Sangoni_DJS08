71a6034f6766b153f615988e90774a3d314b5141
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-10 15:59:30 +0200

    added use navigate and navigated to the host route

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,15 +7,16 @@ export default function Login() {
     const [status, setStatus] = React.useState("idle")
     const [error, setError] = React.useState(null)
     const location = useLocation()
-    console.log(location)
+    const navigate = useNavigate()
 
     function handleSubmit(e) {
         e.preventDefault()
         setStatus("submitting")
         loginUser(loginFormData)
             .then(data => {
-                console.log(data)
+                // console.log(data)
                 setError(null)
+                navigate("/host")
             })
             .catch((err) => {
                 setError(err)
