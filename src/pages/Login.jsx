261198e17a3f571518b65e22a435133ba5885523
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-10 15:56:53 +0200

    set error back to null, update classname for error messages and updated css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,6 +15,7 @@ export default function Login() {
         loginUser(loginFormData)
             .then(data => {
                 console.log(data)
+                setError(null)
             })
             .catch((err) => {
                 setError(err)
@@ -38,12 +39,12 @@ export default function Login() {
         <div className="login-container">
             {
                 location.state?.message && 
-                <h3 className="login-first">{location.state.message}</h3>
+                <h3 className="login-error">{location.state.message}</h3>
             }
             <h1>Sign in to your account</h1>
             {
                 error?.message && 
-                <h3 className="login-first">{error.message}</h3>
+                <h3 className="login-error">{error.message}</h3>
             }
             <form onSubmit={handleSubmit} className="login-form">
                 <input
