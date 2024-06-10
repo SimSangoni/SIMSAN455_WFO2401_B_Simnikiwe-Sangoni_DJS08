e2dfa6d061719d60846abad7c9c9c50bb74b6df1
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-10 11:23:10 +0200

    conditional rendering for back button text

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -17,6 +17,7 @@ export default function VanDetail() {
     )
     
     const search = location.state?.search || ""
+    const type = location.state?.type || "all"
 
     return (
         <div className="van-detail-container">
@@ -24,7 +25,7 @@ export default function VanDetail() {
                 to={`..${search}`}
                 relative="path"
                 className="back-button"
-            >&larr; <span>Back to all vans</span></Link>
+            >&larr; <span>Back to {type} vans</span></Link>
 
             {van ? (
                 <div className="van-detail">
